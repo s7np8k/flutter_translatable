7b90696b6dd50d20e10133aabc0908154db4e099
sebus <s.pietraszek@danmar-computers.com.pl>
2023-11-18 18:44:50 +0100

    init

---
+++ bin/flutter_translatable.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_translatable/flutter_translatable.dart' as flutter_translatable;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_translatable.calculate()}!');
+}
---
+++ test/flutter_translatable_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_translatable/flutter_translatable.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
